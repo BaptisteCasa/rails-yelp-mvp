18aeebf4f6cda3ed8c2bd15cf47e28a5d6117e83
Baptiste Casagrande <baptiste.casagrande@gmail.com>
2025-03-05 15:35:47 +0100

    Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants = [
+  { name: 'Chez Luigi', address: '10 rue de Rome, Paris', phone_number: '01 23 45 67 89', category: 'italian' },
+  { name: 'Sakura', address: '5 avenue des Champs, Paris', phone_number: '01 98 76 54 32', category: 'japanese' },
+  { name: 'Le Gourmet', address: '88 boulevard Haussmann, Paris', phone_number: '01 55 44 33 22', category: 'french' },
+  { name: 'Wok Express', address: '12 rue du Faubourg, Lyon', phone_number: '04 67 89 01 23', category: 'chinese' },
+  { name: 'Belgian Delight', address: '7 place Royale, Bruxelles', phone_number: '02 34 56 78 90', category: 'belgian' }
+]
+
+restaurants.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
